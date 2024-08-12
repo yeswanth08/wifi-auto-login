4e2436cbee5e02048d21b35464c6d0e9b73e75cd
yeswanth08 <curiousyeswanth003@gmail.com>
2024-08-12 16:39:16 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# College WiFi Auto Login
+
+This Chrome extension automates the login process for college's WiFi network by auto-filling and submitting the login form.
+
+## Features
+
+- **Automatic Login**: Automatically fills in your username and password and submits the login form whenever you connect to the college WiFi network.
+- **Quick Access**: Easily toggle the extension on or off from the Chrome toolbar.
+- **Secure**: Your credentials are stored securely in Chrome's local storage.
+
+## Installation
+
+1. **Clone or Download the Repository**  
+   Download this repository as a ZIP file and extract it, or clone it using Git:
+   ```bash
+   git clone https://github.com/yeswanth08/wifi-auto-login.git
+
+2. **Load the Extension in Chrome**
+
+    Open Chrome and navigate to [click](chrome://extensions/.)
+    Enable "Developer mode" by toggling the switch in the top right corner.
+    Click on "Load unpacked" and select the folder where you extracted or cloned the extension.
+
+### The extension will now automatically log you into the college WiFi network whenever the login page is detected.
---
+++ background.js
@@ -0,0 +1,62 @@
+/**
+ * Function responsible to fill the form and submit it
+ * Passed as a string to `executeScript` method
+ */
+function fillFormAndSubmit() {
+  const url = "http://10.10.0.1/24online/webpages/client.jsp";
+
+  const fieldsExist = (document) => {
+    const checkbox = document.getElementById("agreepolicy");
+    const userNameFields = document.getElementsByName("username");
+    const passwordFields = document.getElementsByName("password");
+    const loginButton = document.getElementById("loginbtn");
+
+    console.log(checkbox, userNameFields, passwordFields, loginButton);
+    return (
+      checkbox &&
+      userNameFields.length > 0 &&
+      passwordFields.length > 0 &&
+      loginButton
+    );
+  };
+
+  if (!fieldsExist(document)) {
+    console.log("fields not found");
+    return window.open(url, "_blank");
+  }
+
+  const checkbox = document.getElementById("agreepolicy");
+  if (checkbox) {
+    checkbox.checked = true;
+  } else {
+    console.log("checkbox not found");
+  }
+
+  const userNameFields = document.getElementsByName("username");
+  if (userNameFields.length > 0) {
+    // 1 Enter your registration number
+    userNameFields[0].value = "12210320";
+  }
+
+  const passwordFields = document.getElementsByName("password");
+  if (passwordFields.length > 0) {
+    // 2 Enter your Internet password
+    passwordFields[0].value = "Curious@2003";
+  }
+
+  const loginButton = document.getElementById("loginbtn");
+  if (loginButton) {
+    loginButton.removeAttribute("disabled");
+    loginButton.click();
+  }
+}
+
+/**
+ * Add an event listener to listen to the click event on the extension icon
+ */
+chrome.browserAction.onClicked.addListener(function (tab) {
+  console.log("clicked", tab);
+  chrome.tabs.executeScript(tab.id, {
+    code: "(" + fillFormAndSubmit + ")();",
+  });
+});
